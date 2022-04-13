ab7c983224c26929e094f946ea81b6c0ca5b4dcb
afarukcali <afarukcali@gmail.com>
2022-04-14 01:09:48 +0300

    Init react app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <>Welcome to app</>
+    </div>
+  );
+};
+
+export default App;
