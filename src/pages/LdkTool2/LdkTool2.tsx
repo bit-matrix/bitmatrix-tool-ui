bf51eda8806da5f5a22d4f70c18c234b2544cea5
afarukcali <afarukcali@gmail.com>
2022-04-29 15:20:10 +0300

    Sign transaction script and test.

--- src/pages/LdkTool2/LdkTool2.tsx
+++ src/pages/LdkTool2/LdkTool2.tsx
@@ -1,8 +1,10 @@
-import { craftMultipleRecipientsPset, greedyCoinSelector, UnblindedOutput } from "ldk";
-import { AssetHash, confidential, networks, Psbt, script } from "liquidjs-lib";
+import { AddressInterface, craftMultipleRecipientsPset, greedyCoinSelector, UnblindedOutput } from "ldk";
+import { AssetHash, confidential, networks, Psbt, script, address as liquidAddress } from "liquidjs-lib";
 import { detectProvider, MarinaProvider } from "marina-provider";
 import { useEffect, useState } from "react";
 import { Button, Input, Loader } from "rsuite";
+import { api } from "@bitmatrix/lib";
+import * as ecc from "tiny-secp256k1";
 
 declare global {
   interface Window {
@@ -41,7 +43,32 @@ export const LdkTool2 = () => {
     checkMarinaInstalled();
   }, []);
 
-  const tiero = async () => {
+  const getBlindingKeyByScript = async (script: string): Promise<string> => {
+    try {
+      if (marinaa) {
+        // get addresses from marina
+        const addresses = await marinaa.getAddresses();
+        // find the address of the requested script
+        let found: AddressInterface | undefined;
+
+        addresses.forEach((addr: AddressInterface) => {
+          const currentScript = liquidAddress.toOutputScript(addr.confidentialAddress).toString("hex");
+          if (currentScript === script) {
+            found = addr;
+          }
+        });
+
+        if (!found) throw new Error("no blinding key for script " + script);
+        return found.blindingPrivateKey;
+      }
+
+      return "";
+    } catch (e) {
+      throw e;
+    }
+  };
+
+  const signTransaction = async () => {
     if (marinaa) {
       const coins = await marinaa.getCoins();
 
@@ -52,7 +79,7 @@ export const LdkTool2 = () => {
 
       // 2. add a custom OP_RETURN output to psbt
       pset.addOutput({
-        script: script.compile([script.OPS.OP_RETURN, Buffer.from("hello world", "utf-8")]),
+        script: script.compile([script.OPS.OP_RETURN, Buffer.from("Sampiyon Fenerbahce", "utf-8")]),
         value: confidential.satoshiToConfidentialValue(0),
         asset: AssetHash.fromHex(networks.testnet.assetHash, false).bytes,
         nonce: Buffer.alloc(0),
@@ -62,7 +89,7 @@ export const LdkTool2 = () => {
       const recipients = [
         {
           asset: networks.testnet.assetHash,
-          value: 50000,
+          value: 1907000,
           //P2TR address
           address: "tex1p8la3943se4gc5kxn664ez6d7zq2q34f8mnsfmcylfjzapsyegvfq73avft",
         },
@@ -81,13 +108,45 @@ export const LdkTool2 = () => {
         addFee: true,
       });
 
-      // 6. Sign the transaction's inputs with Marina
-      const signedTx = await marinaa.signTransaction(unsignedTx);
+      // deserialize and inspect the transaction
+      const ptx = Psbt.fromBase64(unsignedTx);
+      //console.log(decoded.TX.toHex());
+
+      // here we sure we are adding only one input and assume the last is our.
+      // In case transaction has input user adds (not marina) be sure to track it down
+      const marinaInputIndex = ptx.data.inputs.length - 1;
+
+      // last output is always the fee output and the one before it, is the marina change output.
+      // keep in mind in case you manipulate transaction manually after calling craftMultipleRecipientsPset
+      const marinaChangeOutputIndex = ptx.data.outputs.length - 2;
+
+      // let's get bliding private first using the marina input script
+      const blindPrivKey = await getBlindingKeyByScript(ptx!.data!.inputs[marinaInputIndex]!.witnessUtxo!.script.toString("hex"));
+
+      // create a map input index => blinding private key
+      // we need this to unblind the utxo data
+      const inputBlindingMap = new Map<number, Buffer>().set(marinaInputIndex, Buffer.from(blindPrivKey, "hex"));
+
+      // create a map output index => blinding PUBLIC (!) key
+      // this is needed to blind the marina change output
+      const outputBlindingMap = new Map<number, Buffer>().set(
+        marinaChangeOutputIndex,
+        // this is the blinding publick key of the change output for marina
+        liquidAddress.fromConfidential(changeAddress.confidentialAddress).blindingKey
+      );
+
+      await ptx.blindOutputsByIndex(Psbt.ECCKeysGenerator(ecc), inputBlindingMap, outputBlindingMap);
+
+      // 7. Sign the transaction's inputs with Marina
+      const signedTx = await marinaa.signTransaction(ptx.toBase64());
+
       // 7. Broadcast the transaction to the network (need to ba added to Marina)
       const finalTx = Psbt.fromBase64(signedTx);
       finalTx.finalizeAllInputs();
 
-      console.log(finalTx.extractTransaction().toHex());
+      const txId = await api.sendRawTransaction(finalTx.extractTransaction().toHex());
+
+      window.open("https://liquid.network/tr/testnet/tx/" + txId, "_blank");
     }
   };
 
@@ -97,7 +156,7 @@ export const LdkTool2 = () => {
       <h6>Address</h6>
       <Input type="text" value={address} onChange={(value: string) => setAddress(value.replace(/\s/g, ""))} />
 
-      <Button className="pool-generator-tool-calculate-button" appearance="primary" size="md" onClick={tiero}>
+      <Button className="pool-generator-tool-calculate-button" appearance="primary" size="md" onClick={signTransaction}>
         xxx
       </Button>
     </div>
