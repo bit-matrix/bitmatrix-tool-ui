f7c979e39cc84c2863e5dda0a912a3c832a476f3
huseyinsaritas <hsynsrtss@gmail.com>
2022-05-12 15:38:25 +0300

    TestPage route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { CommitmentOutputTool } from "./pages/CommitmentOutputTool/CommitmentOut
 import { CreateIssueTool } from "./pages/CreateIssueTool/CreateIssueTool";
 import { LdkTool2 } from "./pages/LdkTool2/LdkTool2";
 import { PoolGenerateTool } from "./pages/PoolGenerateTool/PoolGenerateTool";
+import { TestPage } from "./pages/TestPage/TestPage";
 
 const App = (): JSX.Element => {
   return (
@@ -13,6 +14,7 @@ const App = (): JSX.Element => {
         <Route exact path="/createissue" component={CreateIssueTool} />
         <Route exact path="/ldk" component={LdkTool2} />
         <Route exact path="/commitmentoutput" component={CommitmentOutputTool} />
+        <Route exact path="/test" component={TestPage} />
       </Switch>
     </Router>
   );
