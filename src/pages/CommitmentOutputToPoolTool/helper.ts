fe9efa8a79a04014e1a6f3cf0df1dac279ea61f7
reyhanboydag <reyhanboydag@gmail.com>
2022-06-09 18:19:48 +0300

    helper updated with new validations

--- src/pages/CommitmentOutputToPoolTool/helper.ts
+++ src/pages/CommitmentOutputToPoolTool/helper.ts
@@ -1,26 +1,17 @@
-/* eslint-disable array-callback-return */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { init } from "@bitmatrix/esplora-api-client";
 import { api } from "@bitmatrix/lib";
-import { convertion } from "@script-wiz/lib-core";
 import { TxDetailRPC } from "./models/TxDetailRPC";
-import { TxVInRPC } from "./models/TxVınRPC";
 import { TxVOutRPC } from "./models/TxVOutRPC";
 import WizData, { hexLE } from "@script-wiz/wiz-data";
+import { TxVInRPC } from "./models/TxVınRPC";
+import { convertion } from "@script-wiz/lib-core";
 import axios from "axios";
 import { Pool } from "@bitmatrix/models";
+import Decimal from "decimal.js";
 
-//const testTxId = "d30692c53d6fd85ae594721b5488a8d383e5aae0649194218ce509c0fca9c0eb";
+//const testTxId = "023154e49bd92249d02761e8dd192f32fc31d759e8d83e27b02f274854bf8b49";
 const lbtcAssest = "144c654344aa716d6f3abcc1ca90e5641e4e2a7f633bc09fe3baf64585819a49";
 
-type TxOutsValues = {
-  asset: string;
-  nonce: string;
-};
-
-const commitmentTxFragmentation = async (testTxId: string) => {
-  // fetch tx details with esplora
-  // const txDetails: TxDetail = await esploraClient.tx(testTxId);
+export const commitmentTxOutputsFragmentation = async (testTxId: string) => {
   // fetch tx details with rpc
   const rawTransactionHex = await api.getRawTransaction(testTxId);
   const decodedTransaction: TxDetailRPC = await api.decodeRawTransaction(rawTransactionHex);
@@ -28,50 +19,35 @@ const commitmentTxFragmentation = async (testTxId: string) => {
   //tx outputs
   const outputs: TxVOutRPC[] = decodedTransaction.vout;
 
+  if (outputs.length > 8 && outputs.length < 4) Promise.reject("Outputh length must be smaller than 8 and bigger than 4");
+  const outputCount: WizData | undefined = WizData.fromNumber(outputs.length);
+
   //tx inputs
   const inputs: TxVInRPC[] = decodedTransaction.vin;
 
   if (inputs.length > 12) Promise.reject("Input length must be smaller than 12");
-
-  const inputCount = WizData.fromNumber(inputs.length).hex;
-
-  if (outputs.length > 8) Promise.reject("Outputh length must be smaller than 8");
-  const outputCount = WizData.fromNumber(outputs.length).hex;
-
-  //asset and nonce values pushed an array
-  const txOutValues = outputs.map((out: any) => {
-    if (out.asset && out.commitmentnonce === "") {
-      return { asset: out.asset, nonce: "" };
-    }
-    if (out.assetcommitment && out.commitmentnonce) {
-      return { asset: out.assetcommitment, nonce: out.commitmentnonce };
-    }
-  });
+  const inputCount = WizData.fromNumber(inputs.length);
 
   const cmtTxInOutpoints = inputs.map((inp) => {
     const vout32Byte = convertion.numToLE32(WizData.fromNumber(inp.vout));
     return inp.txid + vout32Byte.hex;
   });
 
-  const nSequences = inputs.map((inp) => {
-    return inp.sequence;
-  });
+  const nSequences = inputs.map((inp) => inp.sequence);
 
   //Every nsequence must equal
   if (nSequences.every((ns) => ns !== nSequences[0])) Promise.reject("Every nSequence must equal");
-  //First output must be OP_RETURN output
+
   const opReturnOutput = outputs[0].scriptPubKey.asm.split(" ");
 
   if (opReturnOutput[0] !== "OP_RETURN") Promise.reject("First output must be OP_RETURN output");
-
-  //poolIdLE + methodCall + publicKey + receivedAmount + config.defaultOrderingFee.hex;
 
   const opReturnOutputScriptHex = opReturnOutput[1];
 
   //poolIdLE (64)
   const poolId = hexLE(opReturnOutputScriptHex.substring(0, 64));
 
-  // methodCall (2)
+  //methodCall (2)
   const methodCall = opReturnOutputScriptHex.substring(64, 66);
 
   // public key (66)
@@ -84,10 +60,34 @@ const commitmentTxFragmentation = async (testTxId: string) => {
   //orderingFees (8)
   const orderingFee = opReturnOutputScriptHex.substring(148, 156);
 
-  if (outputs[1].asset !== lbtcAssest) Promise.reject("Asset must be L-BTC");
+  const cmtOutput1 = outputs[1];
+  const cmtOutput2 = outputs[2];
 
-  const commitmentOutput2AssetId = outputs[2].asset;
+  let changeOutputs: TxVOutRPC[] = [];
 
+  if (methodCall === "01" || methodCall === "02" || methodCall === "04") {
+    if (outputCount.number)
+      for (let i = 3; i < outputCount.number - 1; i++) {
+        changeOutputs.push(outputs[i]);
+      }
+  }
+
+  let cmtOutput3;
+
+  if (methodCall === "03") {
+    cmtOutput3 = outputs[3];
+    if (outputCount.number) {
+      for (let i = 4; i < outputCount.number - 1; i++) {
+        changeOutputs.push(outputs[i]);
+      }
+    }
+  }
+
+  // 6. Commitment out 1 (Calldatadan hemen sonraki output)’in taşıdığı L-BTC değeri 8 byte LE olarak.
+  if (cmtOutput1.asset !== lbtcAssest) Promise.reject("Asset must be L-BTC");
+  const cmtOutput1Value = convertion.numToLE64(WizData.fromNumber(new Decimal(cmtOutput1.value).mul(100000000).toNumber())).hex;
+
+  //   7. Commitment out 2 (Cmt out 1 hemen sonraki output)’nin taşıdığı asset idsi pair1_asset türünden ise 0x03, pair2_asset türünden ise 0x01.
   const poolReq = await axios.get(`https://rocksdb.basebitmatrix.com/pools/${poolId}`);
   const poolDetail: Pool = poolReq.data;
 
@@ -95,44 +95,75 @@ const commitmentTxFragmentation = async (testTxId: string) => {
 
   const pair2Asset = poolDetail.token.asset;
 
-  let pairValue = "";
+  let output2PairValue = "00";
 
-  if (commitmentOutput2AssetId === pair2Asset) pairValue = "01";
-  if (commitmentOutput2AssetId === pair1Asset) pairValue = "03";
+  console.log(cmtOutput2.asset);
+  console.log("pair2Asset", pair2Asset);
+  console.log("pair1Asset", pair1Asset);
 
-  // const slippageToleranceNumber = convertion.LE64ToNum(WizData.fromHex(slippageTolerance)).number;
+  if (cmtOutput2.asset === pair2Asset) output2PairValue = "01";
+  if (cmtOutput2.asset === pair1Asset) output2PairValue = "03";
 
-  // const commitmentOutput1Value = new Decimal(outputs[1].value).mul(100000000).toNumber();
+  //   8. Commitment out 2 ’in taşıdığı asset değeri 8 byte LE olarak.
 
-  // console.log(outputs[2].asset);
-  // console.log(opReturnOutputScriptHex);
-  // console.log(poolId);
-  // console.log(methodCall);
-  // console.log(publicKey);
-  // console.log(slippageTolerance);
-  // console.log(orderingFees);
-  // console.log(commitmentOutput1Value === slippageToleranceNumber);
-  //if (commitmentOutput1Value !== slippageToleranceNumber) throw "Commitment Output Value must equal to Slippage Tolerance Number.";
+  if (cmtOutput2.value === undefined) Promise.reject("Commitment Output Value musn't be confidential.");
+  const cmtOutput2Value = convertion.numToLE64(WizData.fromNumber(new Decimal(cmtOutput2.value).mul(100000000).toNumber())).hex;
+
+  //   9. Commitment out 3 ’ün taşıdığı asset id si pair1_asset türünden ise 0x03, pair2_asset türünden ise 0x01. (bu sadece case 3’ de var, eğer başka bir case ise empty 0x)
+  let cmtOutput3PairValue = "00";
+
+  if (methodCall === "03" && cmtOutput3) {
+    if (cmtOutput3.asset === pair2Asset) cmtOutput3PairValue = "01";
+    if (cmtOutput3.asset === pair1Asset) cmtOutput3PairValue = "03";
+  }
+
+  //   10. Commitment out 3 ’in taşıdığı asset değeri
+
+  let cmtOutput3Value;
+  let cmtOutput3Asset;
+
+  if (cmtOutput3) {
+    cmtOutput3Value = convertion.numToLE64(WizData.fromNumber(new Decimal(cmtOutput3.value).mul(100000000).toNumber())).hex;
+    cmtOutput3Asset = cmtOutput3.asset;
+  }
+
+  const seperatedChangeOutputs = changeOutputs.map((co, index) => {
+    if (co.asset) {
+      return {
+        index: index + 1,
+        asset: "01" + hexLE(co.asset),
+        amount: "01" + new Decimal(co.value).mul(100000000).toString(),
+        nonce: "00",
+        scriptpubkey: WizData.fromNumber(co.scriptPubKey.hex.length / 2).hex + co.scriptPubKey.hex,
+      };
+    }
+
+    return { index: index + 1, asset: co.assetcommitment, amount: co.valuecommitment, nonce: co.commitmentnonce, scriptpubkey: co.scriptPubKey.hex };
+  });
+
+  console.log(seperatedChangeOutputs);
+
+  const changeOutputFinal = seperatedChangeOutputs.map((cof) => {
+    return {
+      index: cof.index,
+      asset: cof.asset + cof.asset,
+      noncScpkey: cof.nonce + cof.scriptpubkey,
+    };
+  });
 
   return {
-    outputs,
-    inputs,
-    txOutValues,
-    cmtTxInOutpoints,
-    nSequences,
-    opReturnOutputScriptHex,
+    cmtOutput1Value,
+    output2PairValue,
+    cmtOutput2Value,
+    cmtOutput3Value,
+    cmtOutput3PairValue,
+    cmtOutput3Asset,
+    changeOutputFinal,
+    seperatedChangeOutputs,
     poolId,
     methodCall,
     publicKey,
     slippageTolerance,
     orderingFee,
-    pair1Asset,
-    pair2Asset,
-    pairValue,
   };
-};
-
-export const commitmentStart = (testTxId: string) => {
-  init("https://electrs.basebitmatrix.com/");
-  commitmentTxFragmentation(testTxId);
 };
