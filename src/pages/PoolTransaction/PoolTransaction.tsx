2a6297de67665efece4fa7dd5a3f3e7f1c96b873
reyhanboydag <reyhanboydag@gmail.com>
2022-07-03 00:32:18 +0300

    updated result

--- src/pages/PoolTransaction/PoolTransaction.tsx
+++ src/pages/PoolTransaction/PoolTransaction.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button, Divider, Input, InputGroup, Loader, Tooltip, Whisper } from "rsuite";
 import CopyIcon from "../../components/Svg/Icons/Copy";
-import { poolData, poolTransaction } from "./helper";
+import { poolTransaction } from "./helper";
 import "./poolTransaction.css";
 
 export const PoolTransaction = () => {
@@ -23,23 +23,24 @@ export const PoolTransaction = () => {
       pool_pair_2_liquidity: cof.pool_pair_2_liquidity,
       commitmentOutput2AssetId: cof.commitmentOutput2AssetId,
       pair_1_asset_id: cof.pair_1_asset_id,
-      user_supply_total: cof.user_supply_total,
-      user_supply_lp_fees: cof.user_supply_lp_fees,
-      user_supply_available: cof.user_supply_available,
-      constant_coefficient: cof.constant_coefficient,
+      pair_2_asset_id: cof.pair_2_asset_id,
+      pool_constant: cof.pool_constant,
       pair_1_coefficient: cof.pair_1_coefficient,
       pair_2_coefficient: cof.pair_2_coefficient,
-      constant_coefficient_downgraded: cof.constant_coefficient_downgraded,
       pool_pair_1_liquidity_downgraded: cof.pool_pair_1_liquidity_downgraded,
       pool_pair_2_liquidity_downgraded: cof.pool_pair_2_liquidity_downgraded,
-      pool_constant: cof.pool_constant,
-      new_pair_2_pool_liquidity_apx_1: cof.new_pair_2_pool_liquidity_apx_1,
-      new_pair_2_pool_liquidity_apx_2: cof.new_pair_2_pool_liquidity_apx_2,
-      user_received_pair_2_apx: cof.user_received_pair_2_apx,
-      payout_additional_fees: cof.payout_additional_fees,
-      user_received_pair_2: cof.user_received_pair_2,
-      new_pool_pair_1_liquidity: cof.new_pool_pair_1_liquidity,
-      new_pool_pair_2_liquidity: cof.new_pool_pair_2_liquidity,
+      user_supply_total: cof.result.user_supply_total,
+      user_supply_lp_fees: cof.result.user_supply_lp_fees,
+      user_supply_available: cof.result.user_supply_available,
+      constant_coefficient: cof.result.constant_coefficient,
+      constant_coefficient_downgraded: cof.result.constant_coefficient_downgraded,
+      new_pair_2_pool_liquidity_apx_1: cof.result.new_pair_2_pool_liquidity_apx_1,
+      new_pair_2_pool_liquidity_apx_2: cof.result.new_pair_2_pool_liquidity_apx_2,
+      user_received_pair_2_apx: cof.result.user_received_pair_2_apx,
+      payout_additional_fees: cof.result.payout_additional_fees,
+      user_received_pair_2: cof.result.user_received_pair_2,
+      new_pool_pair_1_liquidity: cof.result.new_pool_pair_1_liquidity,
+      new_pool_pair_2_liquidity: cof.result.new_pool_pair_2_liquidity,
       lp_liquidty: cof.lp_liquidty,
       new_lp_liquidty: cof.new_lp_liquidty,
     });
@@ -105,11 +106,38 @@ export const PoolTransaction = () => {
               </InputGroup.Button>
             </Whisper>
           </InputGroup>
+          <h6>Pair 1 Supply</h6>
+          <InputGroup>
+            <Input value={result.pair_1_pool_supply} disabled />
+            <Whisper placement="top" trigger="click" speaker={<Tooltip>Pair 1 Asset Id has been copied to clipboard!</Tooltip>}>
+              <InputGroup.Button onClick={() => navigator.clipboard.writeText(result.pair_1_pool_supply || "")}>
+                <CopyIcon width="1rem" height="1rem" />
+              </InputGroup.Button>
+            </Whisper>
+          </InputGroup>
+          <h6>Pair 1 Asset Id</h6>
+          <InputGroup>
+            <Input value={result.pair_2_pool_supply} disabled />
+            <Whisper placement="top" trigger="click" speaker={<Tooltip>Pair 1 Asset Id has been copied to clipboard!</Tooltip>}>
+              <InputGroup.Button onClick={() => navigator.clipboard.writeText(result.pair_2_pool_supply || "")}>
+                <CopyIcon width="1rem" height="1rem" />
+              </InputGroup.Button>
+            </Whisper>
+          </InputGroup>
           <h6>Pair 1 Asset Id</h6>
           <InputGroup>
             <Input value={result.pair_1_asset_id} disabled />
             <Whisper placement="top" trigger="click" speaker={<Tooltip>Pair 1 Asset Id has been copied to clipboard!</Tooltip>}>
               <InputGroup.Button onClick={() => navigator.clipboard.writeText(result.pair_1_asset_id || "")}>
+                <CopyIcon width="1rem" height="1rem" />
+              </InputGroup.Button>
+            </Whisper>
+          </InputGroup>
+          <h6>Pair 2 Asset Id</h6>
+          <InputGroup>
+            <Input value={result.pair_2_asset_id} disabled />
+            <Whisper placement="top" trigger="click" speaker={<Tooltip>Pair 2 Asset Id has been copied to clipboard!</Tooltip>}>
+              <InputGroup.Button onClick={() => navigator.clipboard.writeText(result.pair_2_asset_id || "")}>
                 <CopyIcon width="1rem" height="1rem" />
               </InputGroup.Button>
             </Whisper>
