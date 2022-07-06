82fbf1a5aa6b74291e198fb11f5775a52366f1f4
reyhanboydag <reyhanboydag@gmail.com>
2022-07-06 22:32:17 +0300

    Added Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Nav, Navbar } from "rsuite";
 import { CommitmentOutputTool } from "./pages/CommitmentOutputTool/CommitmentOutputTool";
 import { CommitmentOutputToPoolTool } from "./pages/CommitmentOutputToPoolTool/CommitmentOutputToPool";
 import { CreateIssueTool } from "./pages/CreateIssueTool/CreateIssueTool";
 import { LdkTool2 } from "./pages/LdkTool2/LdkTool2";
 import { PoolGenerateTool } from "./pages/PoolGenerateTool/PoolGenerateTool";
 import { PoolTransaction } from "./pages/PoolTransaction/PoolTransaction";
+import "./App.css";
 
 const App = (): JSX.Element => {
   return (
     <BrowserRouter>
+      <Navbar>
+        <Nav className="nav">
+          <Nav.Item href="/">Home</Nav.Item>
+          <Nav.Item href="/createissue"> Create Issue</Nav.Item>
+          <Nav.Item href="/ldk">Ldk</Nav.Item>
+          <Nav.Item href="/commitmentoutput">Commitment Output</Nav.Item>
+          <Nav.Item href="/commitmentoutputtopool">Commitment Output To Pool</Nav.Item>
+          <Nav.Item href="/pooltransaction">Pool Transaction</Nav.Item>
+        </Nav>
+      </Navbar>
       <Routes>
         <Route path="/" element={<PoolGenerateTool />} />
         <Route path="/createissue" element={<CreateIssueTool />} />
