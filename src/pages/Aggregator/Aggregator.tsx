2df2bd8297d9794e8bc54e24d89dd85b980aa5d1
afarukcali <afarukcali@gmail.com>
2022-07-26 17:09:50 +0300

    Aggreagator with param

--- src/pages/Aggregator/Aggregator.tsx
+++ src/pages/Aggregator/Aggregator.tsx
@@ -1,15 +1,64 @@
 import React from "react";
-import { Button } from "rsuite";
+import { Button, Divider, Input, InputGroup, Tooltip, Whisper } from "rsuite";
 import { createPoolTx } from ".";
+import CopyIcon from "../../components/Svg/Icons/Copy";
 
 export const Aggreagator = () => {
+  const [txId, setTxId] = React.useState<string>("");
+  const [result, setResult] = React.useState<any>();
+
   const run = () => {
-    createPoolTx();
+    const res = createPoolTx(txId);
+    setResult(res);
   };
 
   return (
-    <Button className="pool-generator-tool-calculate-button" appearance="primary" size="md" onClick={run}>
-      Create
-    </Button>
+    <div className="pool-generator-tool-container">
+      <h6>Transaction Id </h6>
+      <Input type="text" value={txId} onChange={(value: string) => setTxId(value.replace(/\s/g, ""))} />
+      <Button className="pool-generator-tool-calculate-button" appearance="primary" size="md" onClick={run}>
+        Create
+      </Button>
+
+      {result && (
+        <div className="pool-generator-tool-result-container">
+          <h6>Inputs</h6>
+          <InputGroup>
+            <Input value={result.inputTemplate} disabled />
+            <Whisper placement="top" trigger="click" speaker={<Tooltip>Inputs has been copied to clipboard!</Tooltip>}>
+              <InputGroup.Button onClick={() => navigator.clipboard.writeText(result.inputTemplate || "")}>
+                <CopyIcon width="1rem" height="1rem" />
+              </InputGroup.Button>
+            </Whisper>
+          </InputGroup>
+
+          <Divider className="pool-generator-tool-divider" />
+
+          <h6>Outputs</h6>
+          <InputGroup>
+            <Input value={result.outputTemplate} disabled />
+            <Whisper placement="top" trigger="click" speaker={<Tooltip>Output template has been copied to clipboard!</Tooltip>}>
+              <InputGroup.Button onClick={() => navigator.clipboard.writeText(result.outputTemplate || "")}>
+                <CopyIcon width="1rem" height="1rem" />
+              </InputGroup.Button>
+            </Whisper>
+          </InputGroup>
+
+          <Divider className="pool-generator-tool-divider" />
+
+          <h6>Witness Template</h6>
+          <InputGroup>
+            <Input value={result.witnessTemplate} disabled />
+            <Whisper placement="top" trigger="click" speaker={<Tooltip>Inputs has been copied to clipboard!</Tooltip>}>
+              <InputGroup.Button onClick={() => navigator.clipboard.writeText(result.witnessTemplate || "")}>
+                <CopyIcon width="1rem" height="1rem" />
+              </InputGroup.Button>
+            </Whisper>
+          </InputGroup>
+
+          <Divider className="pool-generator-tool-divider" />
+        </div>
+      )}
+    </div>
   );
 };
