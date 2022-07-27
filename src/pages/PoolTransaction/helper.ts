2db23bc07175279080a58422fd15023ccd807462
afarukcali <afarukcali@gmail.com>
2022-07-27 14:33:53 +0300

    pool last state updated.

--- src/pages/PoolTransaction/helper.ts
+++ src/pages/PoolTransaction/helper.ts
@@ -30,9 +30,9 @@ export const poolData = {
     block_hash: "7fa6f90f1b8bfe5c9e5aeecda0441cc2814a9374c73ee9e22f8ed1ec6af4bc35",
   },
   unspentTx: {
-    txid: "e3094b74a3db4f83b472531d6564a3e94b956c661fe94296d4da22c7a8624415",
-    block_height: 447661,
-    block_hash: "7fa6f90f1b8bfe5c9e5aeecda0441cc2814a9374c73ee9e22f8ed1ec6af4bc35",
+    txid: "1822c433ea3e83c4c41fe4cd250521f25f6be404c8540285f8e4ecbc6b7eb9e1",
+    block_height: 447671,
+    block_hash: "eba6ab1428fc7cbead50dcbb3520fe08cf0269cbb376916ac4144ed37c70f2e7",
   },
   lastSentPtx: "e3094b74a3db4f83b472531d6564a3e94b956c661fe94296d4da22c7a8624415",
   leafCount: 1,
